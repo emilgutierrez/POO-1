e4335da87c1d5577a63468f426e57ee89be3e0b4
TheZanix <eliasfahuas@liceovvh.cl>
2024-03-15 11:04:15 -0300

    ok

--- variables.js
+++ variables.js
@@ -5,3 +5,17 @@ let local = "Local";
 const constante = "constante";
 
 console.log("Variables: Global"+ global + "- Local: " + local + " - Constante: " + constante);
+
+let num = 4;
+let decimal = 3.1416;
+let string = "texto"
+let bool = true;
+let array = ["perro",20, "Gato", 50];
+let objeto = {
+    Edad:20, 
+    Nombre: "Dany",
+    Telefono: "8116851",
+    Direccion: "El Rosal 2158"
+};
+let indefinida;
+let nula = null;
